38f45c3f88258deea24a729fcf22f921f77e10b1
armandourbina <armandourbina@gmail.com>
2015-03-22 22:16:12 -0430

    Se creó la vista de modificar los colores

--- application/views/color/modificar.php
+++ application/views/color/modificar.php
@@ -1,8 +1,43 @@
-<?php
+<?php $this->load->view('template/header'); ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+<!-- Seccion dinamica -->
+<section class="content-header">
+    <h1>
+        Nuevo color
+        <small>Color</small>
+    </h1>
+
+
+</section>
+
+<section class="content">
+    <div class="row">
+        <!-- left column -->
+        <div class="col-md-6">
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header">
+                    <h3 class="box-title">Color</h3>
+                </div><!-- /.box-header -->
+                <!-- form start -->
+                <form role="form" method="post" action="<?php echo base_url('index.php/color_controller/request/'.$color->id); ?>">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="nombre">Nombre</label>
+                            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Introduzca el nombre del color" value="<?php echo htmlentities($color->nombre); ?>">
+                        </div>
+                        
+                    </div><!-- /.box-body -->
+
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Editar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php $this->load->view('template/footer'); ?>
