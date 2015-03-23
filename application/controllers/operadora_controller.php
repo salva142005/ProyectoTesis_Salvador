67f07a8d842226553a5a4b44f8de22d333ff68f2
armandourbina <armandourbina@gmail.com>
2015-03-23 01:00:46 -0430

    Se agregaron los metodos para el mantenimiento de operadoras

--- application/controllers/operadora_controller.php
+++ application/controllers/operadora_controller.php
@@ -1,40 +1,48 @@
 <?php
 
 class Operadora_Controller extends CI_Controller {
-    
+
     static $view_folder = "operadora";
-}
-   
+
     function __construct() {
         parent::__construct();
-    }
-    
-    function index(){
         $this->load->model('Operadora');
-        $r = $this->Operadora->listar();
-         echo "<pre>";
-        print_r($r->result_array());
-       
     }
-function listar(){
-        $data['Operadoras'] = $this->Operadora->get_Operadoras();
-        $this->load->view(self::$view_folder.'/listar', $data);
+
+    function index() {
+        $this->listar();
     }
-    
-    function crear(){
-        $this->Operadora->set_nombre('Verde Olivo');
-        $this->Operadora->insert();
+
+    function listar() {
+        $data['operadoras'] = $this->Operadora->get_operadoras();
+        $data['titulo'] = "Gestionar Operadoras";
+        $this->load->view(self::$view_folder . '/listar', $data);
     }
     
-    function modificar($id){
+    function request($id = null){
+        $this->Operadora->set_nombre($this->input->post('nombre'));
+        if ($id == null){
+            $this->Operadora->insert(); 
+        } else {
+           $this->Operadora->set_id($id);
+           $this->Operadora->update();
+        }
+        redirect(base_url("index.php/operadora_controller/listar"));
+    }
+
+    function crear() {
+        $this->load->view(self::$view_folder.'/crear');
+    }
+
+    function modificar($id) {
+       $data['operadora']=$this->Operadora->get_operadora_x_id($id);
+       $this->load->view(self::$view_folder.'/modificar', $data);
+    }
+
+    function eliminar($id) {
         $this->Operadora->set_id($id);
-        $this->Operadora->set_nombre('Amarillo Pollito');
-        $this->Operadora->update();
-        
+        $this->Operadora->delete();
+        $this->listar();
     }
-    
-    function eliminar($id){
-         $this->Operadora->set_id($id);
-         $this->Operadora->delete();
-         $this->listar();
-    } 
+
+}
