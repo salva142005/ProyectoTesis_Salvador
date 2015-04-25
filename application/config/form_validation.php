4d18f3f7cc65af1094cbf9e034fd717efa2930aa
Raul Gonzalez <armandourbina@gmail.com>
2015-04-25 14:08:53 -0430

    ejemplos de validación en el form validation

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -3,35 +3,40 @@
 $config = array(
     'valid_usuario' =>
     array(
-        'field' => 'nombre',
-        'label' => 'Nombre',
-        'rules' => 'required|max_length[150]'
+        array(
+            'field' => 'nombre',
+            'label' => 'Nombre',
+            'rules' => 'required|max_length[150]'
+        ),
+        array(
+            'field' => 'email',
+            'label' => 'Email',
+            'rules' => 'required|valid_email|max_length[100]'
+        ),
+        array(
+            'field' => 'confirm-email',
+            'label' => 'Confirmar Email',
+            'rules' => 'required|matches[email]|max_length[100]'
+        ),
+        array(
+            'field' => 'clave',
+            'label' => 'password',
+            'rules' => 'required|max_length[100]'
+        ),
+        array(
+            'field' => 'telefono',
+            'label' => 'telefono',
+            'rules' => 'required|max_length[20]'
+        ),
     ),
-    array(
-        'field' => 'email',
-        'label' => 'Email',
-        'rules' => 'required|valid_email|max_length[100]'
-    ),
-    array(
-        'field' => 'confirm-email',
-        'label' => 'Confirmar Email',
-        'rules' => 'required|matches[email]|max_length[100]'
-    ),
-    array(
-        'field' => 'clave',
-        'label' => 'password',
-        'rules' => 'required|max_length[100]'
-    ),
-    array(
-        'field' => 'telefono',
-        'label' => 'telefono',
-        'rules' => 'required|max_length[20]'
-    ),
-    
     'validacion_equipo' =>
-    array(),
-    array(),
-    array(),
-    array(),
+    array(
+        array(
+            'field' => 'nombre',
+            'label' => 'Descripción del equipo',
+            'rules' => 'required|max_length[225]'
+        ),
+        
+    ),
 );
 
