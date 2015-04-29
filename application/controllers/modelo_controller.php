ccbbbca3417ff3a2cf282559079565bd86bea573
Raul Gonzalez <armandourbina@gmail.com>
2015-04-28 22:56:44 -0430

    No valida cuando se esta editando

--- application/controllers/modelo_controller.php
+++ application/controllers/modelo_controller.php
@@ -7,11 +7,11 @@ class Modelo_Controller extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->model('Modelo');
-         $this->form_validation->set_error_delimiters('<div class="alert alert-danger alert-dismissable"><i class="icon fa fa-ban"></i>', '</div>');
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger alert-dismissable"><i class="icon fa fa-ban"></i>', '</div>');
     }
 
     function index() {
-         $this->listar();
+        $this->listar();
     }
 
     function listar() {
@@ -19,32 +19,34 @@ class Modelo_Controller extends CI_Controller {
         $data['titulo'] = "Gestionar modelos";
         $this->load->view(self::$view_folder . '/listar', $data);
     }
-    
-     function request($id = null){
-         if (!$this->form_validation->run('validar_modelo')){
-            $this->crear();
-            return;
+
+    function request($id = null) {
+        if ($id == null) {
+            if (!$this->form_validation->run('validar_modelo')) {
+                $this->crear();
+                return;
+            }
         }
         $this->Modelo->set_nombre($this->input->post('nombre'));
         $this->Modelo->set_marca_id($this->input->post('marca'));
-        if ($id == null){
-            $this->Modelo->insert(); 
+        if ($id == null) {
+            $this->Modelo->insert();
         } else {
-           $this->Modelo->set_id($id);
-           $this->Modelo->update();
+            $this->Modelo->set_id($id);
+            $this->Modelo->update();
         }
         redirect(base_url("index.php/modelo_controller/listar"));
     }
 
     function crear() {
-       $data['marcas'] = $this->get_marcas();
-       $this->load->view(self::$view_folder.'/crear', $data);
+        $data['marcas'] = $this->get_marcas();
+        $this->load->view(self::$view_folder . '/crear', $data);
     }
 
     function modificar($id) {
         $data['marcas'] = $this->get_marcas();
-        $data['modelo']=$this->Modelo->get_modelo_x_id($id);
-        $this->load->view(self::$view_folder.'/modificar', $data);
+        $data['modelo'] = $this->Modelo->get_modelo_x_id($id);
+        $this->load->view(self::$view_folder . '/modificar', $data);
     }
 
     function eliminar($id) {
@@ -52,8 +54,8 @@ class Modelo_Controller extends CI_Controller {
         $this->Modelo->delete();
         $this->listar();
     }
-    
-    function get_marcas(){
+
+    function get_marcas() {
         $this->load->model('Marca');
         return $this->Marca->get_marcas();
     }
